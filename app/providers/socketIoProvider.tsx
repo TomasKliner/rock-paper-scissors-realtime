c021044666bf8af85a2da18c5a9d227ef57e3dc6
TomasKliner <tomasklinerr@gmail.com>
2025-02-23 00:23:16 +0100

    feat: implement Socket.IO client connection and event handling
    feat: implement Socket.IO client connection and event handling in Home component
    
    feat: set up TypeScript server with Socket.IO integration and game event handling
    
    feat: Added simple logic

--- app/providers/socketIoProvider.tsx
+++ app/providers/socketIoProvider.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useMemo } from 'react';
 import client from "../../utils/socketio";
 import SocketIoContext from "../contexts/socketioContext";
 
 export default function SocketIoProvider({ children }) {
-  return <SocketIoContext.Provider value={{ client }}>{children}</SocketIoContext.Provider>;
+  const value = useMemo(() => ({ client }), []);
+  return <SocketIoContext.Provider value={value}>{children}</SocketIoContext.Provider>;
 }
