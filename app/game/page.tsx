9e8f40c287c4d92b2b585f5a9bf6b00c882620ef
TomasKliner <tomasklinerr@gmail.com>
2025-02-23 18:14:45 +0100

    feat: Created initial game page design

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -23,14 +23,20 @@ export default function GamePage() {
     };
   }, [client]);
 
-  console.log(client);
+  // console.log(client);
+
+  const gameUrlforFriends = `${process.env.NEXT_PUBLIC_PROJECT_URL}/game/${userId}`;
 
   return (
-    <div className="flex justify-center items-center flex-col gap-4 p-8 min-h-screen">
-      <button className="bg-blue-500 rounded-xl shadow p-4 font-bold" onClick={() => client.emit("create-game")}>Create Game</button>
-      <button className="bg-green-500 rounded-xl shadow p-4 font-bold" onClick={() => client.emit("join-game")}>Join Game</button>
-      <div className="text-lg font-bold">User ID: {userId}</div>
-      <QRCode className="bg-white rounded-xl p-4 shadow" value={userId} />
+    <div className="flex justify-center items-center flex-col gap-16 p-8 min-h-screen">
+      <h1 className="text-4xl font-bold">Waiting for other player to join . . .</h1>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-xl">
+          Share this link: <span className="font-bold">{gameUrlforFriends}</span>
+        </h2>
+        <h2 className="text-xl font-bold">or simply share QR code</h2>
+        <QRCode className="bg-white rounded-xl p-4 shadow mt-2" value={gameUrlforFriends} />
+      </div>
     </div>
   );
 }
